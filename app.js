ac7d357ae897cb802b86301028f70de01bfbb5d2
dawidsajewski12-creator <dawidsajewski12@gmail.com>
2025-09-30 09:46:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 
 // Konfiguracja wizualizacji wiatru
 const WIND_VIZ_CONFIG = {
-    PARTICLE_COUNT: 6000,
-    PARTICLE_SPEED_SCALE: 0.2,
-    PARTICLE_LIFESPAN: 1000,  // KRÓCEJ - szybko znikają (było 400)
+    PARTICLE_COUNT: 1000,
+    PARTICLE_SPEED_SCALE: 0.5,
+    PARTICLE_LIFESPAN: 500,  // KRÓCEJ - szybko znikają (było 400)
     PARTICLE_LINE_WIDTH: 1.6,
     PARTICLE_COLOR: "rgba(255, 255, 255, 0.35)",
     GLOW_COLOR: "rgba(200, 220, 255, 0.2)",
